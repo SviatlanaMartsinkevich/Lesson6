5a23ffd93afbc3a34fe949ea01fb95585b5e9535
Sviatlana Martsinkevich <kitunhik@gmail.com>
2021-12-15 22:21:52 +0300

    pageObject

---
+++ src/main/java/baseEntities/BaseTest.java
@@ -0,0 +1,57 @@
+package baseEntities;
+
+import core.ReadProperties;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.config.DriverManagerType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        switch (ReadProperties.getBrowserType().toLowerCase()) {
+            case "chrome":
+                WebDriverManager.getInstance(DriverManagerType.CHROME).setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--disable-gpu"); // оптимизация производительности, без всякой красоты
+                chromeOptions.addArguments("--silent"); //без дополнительного визуального шума
+                chromeOptions.setHeadless(ReadProperties.getHeadless()); // запуск в скрытом режиме, без отображения браузера
+
+                driver = new ChromeDriver(chromeOptions);
+                break;
+            case "firefox":
+                WebDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
+
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.getInstance(DriverManagerType.EDGE).setup();
+
+                driver = new EdgeDriver();
+                break;
+            default:
+                System.out.println("This type of browser is not supported.");
+                break;
+
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get(ReadProperties.getUrl());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
---
+++ src/main/java/core/ReadProperties.java
@@ -0,0 +1,25 @@
+package core;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadProperties {
+    private static Properties properties = null;
+    private static String fileName = "config.properties";
+
+    static{
+        properties = new Properties();
+        try {
+            properties.load(ReadProperties.class.getClassLoader().getResourceAsStream(fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getUrl() { return properties.getProperty("url"); }
+    public static String getBrowserType() { return properties.getProperty("browserType"); }
+    public static boolean getHeadless() { return properties.getProperty("headless").equalsIgnoreCase("true"); }
+    public static String getUsername() { return properties.getProperty("username"); }
+    public static String getPassword() { return properties.getProperty("password"); }
+
+}
