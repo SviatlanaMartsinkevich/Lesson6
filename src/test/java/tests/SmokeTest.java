c3882fd76d5c454202543c1685df22c322547ac9
Sviatlana Martsinkevich <kitunhik@gmail.com>
2022-01-12 20:57:01 +0300

    Лекция

--- src/test/java/tests/SmokeTest.java
+++ src/test/java/tests/SmokeTest.java
@@ -14,9 +14,9 @@ public class SmokeTest extends BaseTest {
     public void loginTest() {
         LoginPage loginPage = new LoginPage(driver);
 
-        loginPage.getEmailField().sendKeys(ReadProperties.getUsername());
-        loginPage.getPasswordField().sendKeys(ReadProperties.getPassword());
-        loginPage.getLoginButton().click();
+        loginPage.emailField.sendKeys(ReadProperties.getUsername());
+        loginPage.passwordField.sendKeys(ReadProperties.getPassword());
+        loginPage.loginButton.click();
 
         DashboardPage dashboardPage = new DashboardPage(driver);
         waits.waitForVisibility(dashboardPage.getAddProjectButton());
@@ -28,13 +28,5 @@ public class SmokeTest extends BaseTest {
     public void flakyLoginTest() {
         LoginPage loginPage = new LoginPage(driver);
 
-        loginPage.getEmailField().sendKeys(ReadProperties.getUsername());
-        loginPage.getPasswordField().sendKeys(ReadProperties.getPassword());
-        loginPage.getLoginButton().click();
-
-        DashboardPage dashboardPage = new DashboardPage(driver);
-        waits.waitForVisibility(dashboardPage.getAddProjectButton());
-
-        Assert.assertTrue(dashboardPage.isPageOpened());
     }
 }
